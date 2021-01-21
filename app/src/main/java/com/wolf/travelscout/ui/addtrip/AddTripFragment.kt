6c0a6d668aff09b7fdb643a0ce6b98f10d7997c8
Fareez <fareez.mohamad@hexainitiative.com>
2021-01-21 23:11:05 +0800

    Add country spinner

--- app/src/main/java/com/wolf/travelscout/ui/addtrip/AddTripFragment.kt
+++ app/src/main/java/com/wolf/travelscout/ui/addtrip/AddTripFragment.kt
@@ -1,11 +1,14 @@
 package com.wolf.travelscout.ui.addtrip
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.wolf.travelscout.R
+import kotlinx.android.synthetic.main.fragment_add_trip.*
 
 class AddTripFragment : Fragment() {
 
@@ -13,6 +16,25 @@ class AddTripFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_trip, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val name = arrayOf(
+                "Malaysia",
+                "Singapore"
+        )
+
+        val adapter = ArrayAdapter<String>(requireContext(), R.layout.material_spinner_item, name)
+        filled_exposed_dropdown.setAdapter(adapter)
+        filled_exposed_dropdown.setOnItemClickListener { parent, view, position, id ->
+            Log.i("Position", position.toString())
+
+            val a = filled_exposed_dropdown.text
+            Log.i("Position", a.toString())
+        }
+
     }
 
 }
